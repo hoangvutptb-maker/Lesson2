886648717169fd3759dd0a38e1142b4b93f15f90
hoangvu <hoangvutptb@gmail.com>
2026-05-28 22:14:55 +0700

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lesson2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống tên sách")]
+        public string Name { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Giá phải lớn hơn 0")]
+        public double Price { get; set; }
+    }
+}
